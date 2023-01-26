8c8d365713e8d52668f001257beae360104ae331
Sarah_alhajmuhammed <75433758+sarahaddad@users.noreply.github.com>
2023-01-26 20:10:12 +0300

    create add note method

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,7 +1,22 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:noteapp/constant.dart';
+import 'package:noteapp/models/note_model.dart';
 
 import 'add_note_states.dart';
 
 class AddNoteCubit extends Cubit<AddNotesStates> {
   AddNoteCubit() : super(AddNoteIntial());
+
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailure(errMsg: e.toString()));
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:noteapp/constant.dart';
+import 'package:noteapp/models/note_model.dart';
 import 'package:noteapp/views/note_view.dart';
 
 import 'models/note_model.g.dart';
 
 void main() async {
   await Hive.initFlutter();
-  await Hive.openBox(kNotesBox);
+  await Hive.openBox<NoteModel>(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
   runApp(const MyApp());
 }
