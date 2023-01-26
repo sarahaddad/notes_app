219ac1187fb6aedc3aa72e55fde0904c9b43efb4
Sarah_alhajmuhammed <75433758+sarahaddad@users.noreply.github.com>
2023-01-27 01:25:21 +0300

    loading button,AbsorbPointer to freeze the screen

--- lib/views/widgets/custom_textbutton.dart
+++ lib/views/widgets/custom_textbutton.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:noteapp/constant.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
+  final bool isLoading;
   final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
@@ -16,15 +17,22 @@ class CustomButton extends StatelessWidget {
             borderRadius: BorderRadius.circular(
               8,
             )),
-        child: const Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
+        child: Center(
+          child: isLoading
+              ? const SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ))
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
         ),
       ),
     );
